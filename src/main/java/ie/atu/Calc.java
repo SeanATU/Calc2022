f69217c869b032e4e18c0d56d49fcdc2e4f78e90
SeanATU <g00292129@atu.ie>
2022-11-11 22:51:56 +0000

    added add method

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,14 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args){
+
         System.out.println("Pushing code");
+
+    }
+    public static void add(){
+        System.out.println("This is the add method");
     }
 }
